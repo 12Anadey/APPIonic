4ed72bcbdc0ef89c81d3f98541dc6f97cbe659a6
Anadey <fabiolaanadeyriosgarcia@gmail.com>
2026-04-16 10:29:29 -0600

    Agregada plataforma Android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.anadey.miapp',
+  appName: 'MyAppPersonal',
+  webDir: 'www'
+};
+
+export default config;
